5f0b1827fb2e23ac8f256170d9b961daf926e3cf
MEFRREEX <mefrreex@gmail.com>
2024-03-29 12:53:42 +0200

    refactor: ChestSwitchMode

--- src/main/java/com/mefrreex/chestcreator/chest/ChestSwitchMode.java
+++ src/main/java/com/mefrreex/chestcreator/chest/ChestSwitchMode.java
@@ -2,29 +2,28 @@ package com.mefrreex.chestcreator.chest;
 
 import cn.nukkit.Player;
 import com.mefrreex.chestcreator.chest.ChestManager.PlayerChest;
-import com.mefrreex.chestcreator.utils.TriConsumer;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public enum ChestSwitchMode {
-    REOPEN((player, playerChest, targetChest) -> {
-        playerChest.inventory().close(player);
-        new Timer().schedule(new TimerTask() {
-            public void run() {
-                targetChest.send(player);
-            }
-        }, 500);
-    }),
-    DEFAULT((player, playerChest, targetChest) -> targetChest.send(player));
+    REOPEN {
+        @Override
+        public void process(Player player, PlayerChest playerChest, Chest targetChest) {
+            playerChest.inventory().close(player);
+            new Timer().schedule(new TimerTask() {
+                public void run() {
+                    targetChest.send(player);
+                }
+            }, 500);
+        }
+    },
+    DEFAULT {
+        @Override
+        public void process(Player player, PlayerChest playerChest, Chest targetChest) {
+            targetChest.send(player);
+        }
+    };
 
-    private final TriConsumer<Player, PlayerChest, Chest> processor;
-
-    ChestSwitchMode(TriConsumer<Player, PlayerChest, Chest> processor) {
-        this.processor = processor;
-    }
-
-    public void process(Player player, PlayerChest playerChest, Chest targetChest) {
-        processor.accept(player, playerChest, targetChest);
-    }
+    public abstract void process(Player player, PlayerChest playerChest, Chest targetChest);
 }
