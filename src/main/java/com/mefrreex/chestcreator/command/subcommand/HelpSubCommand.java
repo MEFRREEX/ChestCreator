559680c5c7866c90ac1d1073a2b48d2d292a2ac0
MEFRREEX <mefrreex@gmail.com>
2024-03-30 10:31:55 +0200

    feat: reload command

--- src/main/java/com/mefrreex/chestcreator/command/subcommand/HelpSubCommand.java
+++ src/main/java/com/mefrreex/chestcreator/command/subcommand/HelpSubCommand.java
@@ -1,21 +1,26 @@
 package com.mefrreex.chestcreator.command.subcommand;
 
+import cn.nukkit.command.CommandSender;
 import com.mefrreex.chestcreator.command.BaseCommand;
 import com.mefrreex.chestcreator.command.BaseSubCommand;
 import com.mefrreex.chestcreator.utils.Language;
-
-import cn.nukkit.command.CommandSender;
 
 public class HelpSubCommand extends BaseSubCommand {
 
     public HelpSubCommand(BaseCommand command) {
         super("help", "Help", command);
+        this.setPermission("chestcreator.help");
     }
 
     @Override
     public boolean execute(CommandSender sender, String commandLabel, String label, String[] args) {
+        if (!this.testPermission(sender)) {
+            return false;
+        }
+
         StringBuilder builder = new StringBuilder();
         builder.append(Language.get("command-help-available"));
+
         for (BaseSubCommand sub : this.getCommand().getSubcommands().values()) {
             if (sub.testPermission(sender)) {
                 String name = sub.getName();
@@ -23,6 +28,7 @@ public class HelpSubCommand extends BaseSubCommand {
                 builder.append("\n" + Language.get("command-help-format", commandLabel, name, description));
             }
         }
+
         sender.sendMessage(builder.toString());
         return true;
     }
